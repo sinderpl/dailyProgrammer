0e86357f2e8c93b8dc7f1fc44cc34740e7be9362
Alan <18658841+sinderpl@users.noreply.github.com>
2018-07-06 08:06:23 +0100

    Create FrogLeafCounter.java

---
+++ FrogLeafCounter.java
@@ -0,0 +1,80 @@
+/**
+A small frog wants to get to the other side of a river. The frog is initially located on one bank of the river (position 0) and wants to get to the opposite bank (position X+1). Leaves fall from a tree onto the surface of the river.
+
+You are given an array A consisting of N integers representing the falling leaves. A[K] represents the position where one leaf falls at time K, measured in seconds.
+
+The goal is to find the earliest time when the frog can jump to the other side of the river. The frog can cross only when leaves appear at every position across the river from 1 to X (that is, we want to find the earliest moment when all the positions from 1 to X are covered by leaves). You may assume that the speed of the current in the river is negligibly small, i.e. the leaves do not change their positions once they fall in the river.
+
+For example, you are given integer X = 5 and array A such that:
+
+  A[0] = 1
+  A[1] = 3
+  A[2] = 1
+  A[3] = 4
+  A[4] = 2
+  A[5] = 3
+  A[6] = 5
+  A[7] = 4
+In second 6, a leaf falls into position 5. This is the earliest time when leaves appear in every position across the river.
+
+Write a function:
+
+class Solution { public int solution(int X, int[] A); }
+
+that, given a non-empty array A consisting of N integers and integer X, returns the earliest time when the frog can jump to the other side of the river.
+
+If the frog is never able to jump to the other side of the river, the function should return −1.
+
+For example, given X = 5 and array A such that:
+
+  A[0] = 1
+  A[1] = 3
+  A[2] = 1
+  A[3] = 4
+  A[4] = 2
+  A[5] = 3
+  A[6] = 5
+  A[7] = 4
+the function should return 6, as explained above.
+
+Assume that:
+
+N and X are integers within the range [1..100,000];
+each element of array A is an integer within the range [1..X].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(X) (not counting the storage required for input arguments).
+**/
+
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int X, int[] A) {
+        int value = 0;
+        //Counter for whether the corresponding leafs are preset
+        boolean[] leafPresent = new boolean[X+1];
+        
+        //Iterate over the leafs
+        for(int y = 0; y < A.length; y++)
+        {
+            //If the leaf is not already set to true (for the counter value)
+            //And if value is smaller than X specified
+            if(!leafPresent[A[y]] && A[y] <= X)
+            {
+                leafPresent[A[y]] = true;
+                value++;
+            }
+            if(value == X)
+            {
+                return y;
+            }
+        }
+        //Only returned if it is not possible to get to other side
+        return -1;
+    }
+}
