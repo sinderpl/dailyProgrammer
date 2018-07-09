56188239812fddfb2affc88d327edd82c3f3c127
Alan <18658841+sinderpl@users.noreply.github.com>
2018-07-09 12:10:31 +0100

    Create CountDiv.go

---
+++ CountDiv.go
@@ -0,0 +1,41 @@
+/**
+Write a function:
+
+func Solution(A int, B int, K int) int
+
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+{ i : A ≤ i ≤ B, i mod K = 0 }
+
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+Assume that:
+
+A and B are integers within the range [0..2,000,000,000];
+K is an integer within the range [1..2,000,000,000];
+A ≤ B.
+Complexity:
+
+expected worst-case time complexity is O(1);
+expected worst-case space complexity is O(1).
+**/
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A int, B int, K int) int {
+    if K == 0{
+        return 0
+    }else{
+        if A % K == 0{
+            return ( B / K ) - ( A / K) + 1
+        }else{
+            return  ( B / K ) - ( A / K)
+        }
+    }
+}
