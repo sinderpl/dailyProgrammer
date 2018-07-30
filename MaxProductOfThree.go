aab9b9858c992800906c96237078a45ee76beb32
Alan <18658841+sinderpl@users.noreply.github.com>
2018-07-30 08:48:20 +0100

    Create MaxProductOfThree.go

---
+++ MaxProductOfThree.go
@@ -0,0 +1,65 @@
+/**
+  A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+contains the following example triplets:
+
+(0, 1, 2), product is −3 * 1 * 2 = −6
+(1, 2, 4), product is 1 * 2 * 5 = 10
+(2, 4, 5), product is 2 * 5 * 6 = 60
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
+
+func Solution(A []int) int
+
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+For example, given array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+
+Assume that:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+Complexity:
+
+expected worst-case time complexity is O(N*log(N));
+expected worst-case space complexity is O(1) (not counting the storage required for input arguments).
+**/
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+import "sort"
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+    result := 0
+    sort.Ints(A)
+    
+    maxA := A[len(A) -1] * A[len(A) -2] * A[len(A) -3]
+    maxB := A[len(A) -1] * A[0] * A[1]
+    if maxA > maxB{
+        result = maxA
+    }else{
+        result = maxB
+    }
+    return result
+}
