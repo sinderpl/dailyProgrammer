af07257d4234dbb3aab05ff56aa3d3cffbf84d27
Alan <18658841+sinderpl@users.noreply.github.com>
2018-07-31 08:15:21 +0100

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,60 @@
+/**
+An array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+
+A[P] + A[Q] > A[R],
+A[Q] + A[R] > A[P],
+A[R] + A[P] > A[Q].
+For example, consider array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+Triplet (0, 2, 4) is triangular.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+
+For example, given array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+the function should return 1, as explained above. Given array A such that:
+
+  A[0] = 10    A[1] = 50    A[2] = 5
+  A[3] = 1
+the function should return 0.
+
+Assume that:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(N*log(N));
+expected worst-case space complexity is O(N) (not counting the storage required for input arguments).
+**/
+// you can also use imports, for example:
+// import java.util.*;
+import java.util.Arrays;
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        if(A.length < 3)
+        {
+            return 0;
+        }
+        Arrays.sort(A);
+        for( int x = 2 ; x < A.length; x++)
+        {
+            if(A[x] < A[x-2] + A[x-1])
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
